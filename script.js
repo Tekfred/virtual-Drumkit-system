1bbd0e59d19537989afe26143adbd41e20dbd087
Tekfred <asagodfred101@gmail.com>
2025-10-27 14:03:56 +0000

    created ui using tailwind with javascript

---
+++ script.js
@@ -0,0 +1,51 @@
+
+const clickDrumButton = document.querySelectorAll('.drum-btn').length;
+
+for(let i = 0; i <clickDrumButton; i++){
+    document.querySelectorAll('.drum-btn')[i].addEventListener("click", function() {
+    
+       var buttonInnerHTML = this.innerHTML;
+
+       switch (buttonInnerHTML){
+        case "w":
+            var snare = new Audio('/media/sounds/snare.mp3');
+            snare.play();
+        break;
+
+            case "a":
+            var crash = new Audio('/media/sounds/crash.mp3');
+           crash.play();
+            break;
+
+            case "s":
+            var tom1 = new Audio('/media/sounds/tom-1.mp3');
+            tom1.play();
+            break;
+
+            case "d":
+                var tom2 = new Audio('/media/sounds/tom-2.mp3');
+               tom2.play();
+            break;
+
+             case "j":
+                var tom3 = new Audio('/media/sounds/tom-3.mp3');
+               tom3.play();
+            break;
+
+            case "k":
+                var kickb = new Audio('/media/sounds/kick-bass.mp3');
+               kickb.play();
+            break;
+
+            case "l":
+                var tom4 = new Audio('/media/sounds/tom-4.mp3');
+               tom4.play();
+            break;
+
+
+        default: console.log(buttonInnerHTML); 
+       }
+});
+}
+
+ 
